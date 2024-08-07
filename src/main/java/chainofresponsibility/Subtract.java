35bff5cc24434f1cb8dbf3d578f3f514af5214fa
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2024-08-07 16:00:55 +0330

    chain of responsibility package was completed.

---
+++ src/main/java/chainofresponsibility/Subtract.java
@@ -0,0 +1,18 @@
+package chainofresponsibility;
+
+public class Subtract implements Chain {
+    private Chain nextInChain;
+
+
+    @Override
+    public void setNextInChain(Chain nextInChain) {
+        this.nextInChain = nextInChain;
+    }
+
+    @Override
+    public int calculate(Function function) {
+        if (function.getFunction().equals("-")) {
+            return function.getNumber1() - function.getNumber2();
+        } else return nextInChain.calculate(function);
+    }
+}
